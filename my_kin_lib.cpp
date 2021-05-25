90df21ab65f71e7c6fbba670ec448dc3aeb2de57
Ilya <idprodma@miem.hse.ru>
2021-05-25 12:19:41 +0300

    add comments

--- my_kin_lib.cpp
+++ my_kin_lib.cpp
@@ -1,7 +1,6 @@
 #include "my_kin_lib.h"
 #include <Adafruit_PWMServoDriver.h>
 
-
 #define L1 40
 #define L2 100
 #define L3 100
@@ -25,44 +24,45 @@
 
 
 
-//STATIONARY POSITIONS
+// СТАЦИОНАРНОЕ ПОЛОЖЕНИЕ СТОЯ
 void stand_stationary(Adafruit_PWMServoDriver pwm)
 {
-    pwm.setPWM(0, 0, default0-200);//передняя правая
+    pwm.setPWM(0, 0, default0-200); // Передняя правая
     pwm.setPWM(1, 0, default1+100);
     pwm.setPWM(2, 0, default2);
 
-    pwm.setPWM(3, 0, default3+150);//передняя левая
+    pwm.setPWM(3, 0, default3+150); // Передняя левая
     pwm.setPWM(4, 0, default4-100);
     pwm.setPWM(5, 0, default5);
 
-    pwm.setPWM(6, 0, default6-200);//задняя правая
+    pwm.setPWM(6, 0, default6-200); // Задняя правая
     pwm.setPWM(7, 0, default7+100);
     pwm.setPWM(8, 0, default8);
 
-    pwm.setPWM(9, 0, default9+200);//задняя левая
+    pwm.setPWM(9, 0, default9+200); // Задняя левая
     pwm.setPWM(10, 0, default10-100);
     pwm.setPWM(11, 0, default11);
-    //----------------------------------------------
 }
 
+
+// МЕТОД ПЕРЕХОДА ИЗ ПОЛОЖЕНИЯ СИДЯ В ПОЛОЖЕНИЕ СТОЯ
 void stand_up_from_down(Adafruit_PWMServoDriver pwm) 
 {
   for (int i = 0; i < 100; i++)
     {
-      pwm.setPWM(0, 0, default0-i*2);//передняя правая
+      pwm.setPWM(0, 0, default0-i*2); // Передняя правая
       pwm.setPWM(1, 0, default1+i);
       pwm.setPWM(2, 0, default2);
   
-      pwm.setPWM(3, 0, default3+i*1.5);//передняя левая
+      pwm.setPWM(3, 0, default3+i*1.5); // Передняя левая
       pwm.setPWM(4, 0, default4-i);
       pwm.setPWM(5, 0, default5);
   
-      pwm.setPWM(6, 0, default6-i*2);//задняя правая
+      pwm.setPWM(6, 0, default6-i*2); // Задняя правая
       pwm.setPWM(7, 0, default7+i);
       pwm.setPWM(8, 0, default8);
   
-      pwm.setPWM(9, 0, default9+i*2);//задняя левая
+      pwm.setPWM(9, 0, default9+i*2); // Задняя левая
       pwm.setPWM(10, 0, default10-i);
       pwm.setPWM(11, 0, default11);
       delay(10);
@@ -71,46 +71,46 @@ void stand_up_from_down(Adafruit_PWMServoDriver pwm)
    stand_stationary(pwm);
 }
 
+
+// СТАЦИОНАРНОЕ ПОЛОЖЕНИЕ СИДЯ
 void sit_stationary(Adafruit_PWMServoDriver pwm)
 {
-    pwm.setPWM(0, 0, default0); //передняя правая
+    pwm.setPWM(0, 0, default0); // Передняя правая
     pwm.setPWM(1, 0, default1);
     pwm.setPWM(2, 0, default2);
 
-    pwm.setPWM(3, 0, default3); //передняя левая
+    pwm.setPWM(3, 0, default3); // Передняя левая
     pwm.setPWM(4, 0, default4);
     pwm.setPWM(5, 0, default5);
 
-    pwm.setPWM(6, 0, default6);//задняя правая
+    pwm.setPWM(6, 0, default6); // Задняя правая
     pwm.setPWM(7, 0, default7);
     pwm.setPWM(8, 0, default8);
 
-    pwm.setPWM(9, 0, default9);//задняя левая
+    pwm.setPWM(9, 0, default9); // Задняя левая
     pwm.setPWM(10, 0, default10);
     pwm.setPWM(11, 0, default11);
-    
-    //+++++++++++++++++++++++++++++++++++++++++++
 }
 
 
-// МЕТОД ПРИСЕДАНИЯ ИЗ СТАЦИОНАРНОГО ПОЛОЖЕНИЯ
+// МЕТОД ПРИСЕДАНИЯ ИЗ ПОЛОЖЕНИЯ СТОЯ
 void sit_down_from_stationary(Adafruit_PWMServoDriver pwm) 
 {
   for (int i = 100; i > 0; i--)
     {
-      pwm.setPWM(0, 0, default0-i*2);//передняя правая
+      pwm.setPWM(0, 0, default0-i*2); // Передняя правая
       pwm.setPWM(1, 0, default1+i);
       pwm.setPWM(2, 0, default2);
   
-      pwm.setPWM(3, 0, default3+i*1.5);//передняя левая
+      pwm.setPWM(3, 0, default3+i*1.5); // Передняя левая
       pwm.setPWM(4, 0, default4-i);
       pwm.setPWM(5, 0, default5);
   
-      pwm.setPWM(6, 0, default6-i*2);//задняя правая
+      pwm.setPWM(6, 0, default6-i*2); // Задняя правая
       pwm.setPWM(7, 0, default7+i);
       pwm.setPWM(8, 0, default8);
   
-      pwm.setPWM(9, 0, default9+i*2);//задняя левая
+      pwm.setPWM(9, 0, default9+i*2); // Задняя левая
       pwm.setPWM(10, 0, default10-i);
       pwm.setPWM(11, 0, default11);
       delay(10);
@@ -143,6 +143,7 @@ void tilt_right(Adafruit_PWMServoDriver pwm)
     tilt_right_stationary(pwm);
 }
 
+// СТАЦИОНАРНОЕ ПОЛОЖЕНИЕ НАКЛОНА ВПРАВО
 void tilt_right_stationary(Adafruit_PWMServoDriver pwm)
 {
       pwm.setPWM(2, 0, default2+40); // Передняя правая
@@ -158,6 +159,7 @@ void tilt_right_stationary(Adafruit_PWMServoDriver pwm)
       pwm.setPWM(10, 0, default10-40);
       pwm.setPWM(9, 0, default9+40);
 }
+
 
 // ВОЗВРАЩЕНИЕ ИЗ НАКЛОНА ВПРАВО
 void sit_stationary_from_tilt_right(Adafruit_PWMServoDriver pwm)
@@ -182,6 +184,7 @@ void sit_stationary_from_tilt_right(Adafruit_PWMServoDriver pwm)
   sit_stationary(pwm);
 }
 
+
 // НАКЛОН ВЛЕВО
 void tilt_left(Adafruit_PWMServoDriver pwm)
 {
@@ -204,6 +207,7 @@ void tilt_left(Adafruit_PWMServoDriver pwm)
     tilt_left_stationary(pwm);
 }
 
+
 // СТАЦИОНАРНОЕ ПОЛОЖЕНИЕ НАКЛОНА ВЛЕВО
 void tilt_left_stationary(Adafruit_PWMServoDriver pwm)
 {
@@ -220,6 +224,7 @@ void tilt_left_stationary(Adafruit_PWMServoDriver pwm)
       pwm.setPWM(10, 0, default10-40); // Задняя левая
       pwm.setPWM(11, 0, default11+40);
 }
+
 
 // ВОЗВРАЩЕНИЕ ИЗ НАКЛОНА ВЛЕВО
 void sit_stationary_from_tilt_left(Adafruit_PWMServoDriver pwm)
